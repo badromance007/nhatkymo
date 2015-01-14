4f4ddade7d0b749ae653591584f9801b872c5c20
tuong <tuong@tuong-Aspire.(none)>
2015-01-14 20:03:47 +0700

    Add diaries view

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,14 +1,24 @@
 class DiariesController < ApplicationController
+  before_action :find_diary, only: [:show, :edit, :update, :destroy]
   def index
+    @diaries = Diary.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @diary = Diary.new
   end
 
   def create
+    @diary = Diary.new(diary_params)
+
+    if @diary.save
+      redirect_to @diary 
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,8 +33,10 @@ class DiariesController < ApplicationController
   private
 
     def find_diary
+      @diary = Diary.find(params[:id])
     end
 
     def diary_params
+      params.require(:diary).permit(:title, :content)
     end
 end
