20d82df130f0ab990d7e5687449090980ceba830
tuong <tuong@tuong-Aspire.(none)>
2015-01-25 12:47:36 +0700

    remove second time to test 500 server error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,32 +13,7 @@ class UsersController < ApplicationController
     # get the number of comments
     @fb_comments = fb_comments_data["share"]["comment_count"]
 
-    # second time
-    url_commented_ons = []
-    @user_diaries.each do |diary |
-      url_commented_ons << diary_url(diary)
-    end
-
-    fb_comments_urls = []
-    url_commented_ons.each do | url |
-      fb_comments_urls << "https://graph.facebook.com/v2.1/?fields=share{comment_count}&id="+url
-    end
-    fb_comments_jsons = []
-    fb_comments_urls.each do | graph_url |
-      fb_comments_jsons << open(graph_url).read
-    end
-    fb_comments_datas = []
-    fb_comments_jsons.each do | graph_url_json |
-      fb_comments_datas << ActiveSupport::JSON.decode(graph_url_json)
-    end
-
-    @fb_comments_number = 0
-    # get the number of comments
-    fb_comments_datas.each do | data | 
-      if data.length > 1
-        @fb_comments_number += data["share"]["comment_count"].to_i
-      end
-    end
+    
     
 
 
