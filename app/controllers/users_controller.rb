434cb3f2abdcddf54cdb8f2ab2a14562a0217daa
tuong <tuong@tuong-Aspire.(none)>
2015-01-25 12:51:30 +0700

    Delete first time to test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,7 @@ class UsersController < ApplicationController
     @user_diaries = Diary.where(user_id: find_user).order("created_at desc")
      
 
-    #first time
-    url_commented_on = "http://nhatkymo.herokuapp.com/diaries/23"
-    fb_comments_url = "https://graph.facebook.com/v2.1/?fields=share{comment_count}&id="+url_commented_on
-    fb_comments_json = open(fb_comments_url).read
-    fb_comments_data = ActiveSupport::JSON.decode(fb_comments_json)
-
-    # get the number of comments
-    @fb_comments = fb_comments_data["share"]["comment_count"]
-
+    
     
     
 
