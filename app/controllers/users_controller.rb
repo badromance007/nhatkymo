e0c75e9ec7e5cd267815ab24d41b9245671bb929
tuong <tuong@tuong-Aspire.(none)>
2015-01-25 16:04:09 +0700

    loop through to display all facebook comments and encode url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,51 +5,51 @@ class UsersController < ApplicationController
      
 
     #first time
-    url_commented_on = "http://nhatkymo.herokuapp.com/diaries/22"
-    fb_comments_url = "https://graph.facebook.com/v2.1/?fields=share{comment_count}&id="+url_commented_on
+    #url_commented_on = "http://nhatkymo.herokuapp.com/diaries/22"
+    #fb_comments_url = "https://graph.facebook.com/v2.1/?fields=share{comment_count}&id="+url_commented_on
+    #url = URI.parse(URI.encode(fb_comments_url.strip))
     #fb_comments_json = open(fb_comments_url).read
     #fb_comments_data = ActiveSupport::JSON.decode(fb_comments_json)
 
-    require 'restclient'
-
-    url = URI.parse(URI.encode(fb_comments_url.strip))
+    #url = URI.parse(URI.encode(fb_comments_url.strip))
     
     #user_info = RestClient.get(url, "User-Agent" => "Ruby")
-    user_info = open(url).read
+    #user_info = open(url).read
 
-    fb_comments_data = ActiveSupport::JSON.decode(user_info)
+    #fb_comments_data = ActiveSupport::JSON.decode(user_info)
 
     # get the number of comments
-    @fb_comments = fb_comments_data["share"]["comment_count"]
+    #@fb_comments = fb_comments_data["share"]["comment_count"]
 
 
 
     # second time
-    # url_commented_ons = []
-    # @user_diaries.each do |diary |
-    #   url_commented_ons << diary_url(diary)
-    # end
+     url_commented_ons = []
+     @user_diaries.each do |diary |
+       url_commented_ons << diary_url(diary)
+     end
 
-    # fb_comments_urls = []
-    # url_commented_ons.each do | url |
-    #   fb_comments_urls << "https://graph.facebook.com/v2.1/?fields=share{comment_count}&id="+url
-    # end
-    # fb_comments_jsons = []
-    # fb_comments_urls.each do | graph_url |
-    #   fb_comments_jsons << open(graph_url).read
-    # end
-    # fb_comments_datas = []
-    # fb_comments_jsons.each do | graph_url_json |
-    #   fb_comments_datas << ActiveSupport::JSON.decode(graph_url_json)
-    # end
+     fb_comments_urls = []
+     url_commented_ons.each do | url |
+       fb_comments_urls << "https://graph.facebook.com/v2.1/?fields=share{comment_count}&id="+url
+     end
+     fb_comments_jsons = []
+     fb_comments_urls.each do | graph_url |
+       url = URI.parse(URI.encode(graph_url.strip))
+       fb_comments_jsons << open(url).read
+     end
+     fb_comments_datas = []
+     fb_comments_jsons.each do | graph_url_json |
+       fb_comments_datas << ActiveSupport::JSON.decode(graph_url_json)
+     end
 
-    # @fb_comments_number = 0
-    # # get the number of comments
-    # fb_comments_datas.each do | data | 
-    #   if data.length > 1
-    #     @fb_comments_number += data["share"]["comment_count"].to_i
-    #   end
-    # end
+     @fb_comments_number = 0
+     # get the number of comments
+     fb_comments_datas.each do | data | 
+       if data.length > 1
+         @fb_comments_number += data["share"]["comment_count"].to_i
+       end
+     end
     
 
 
