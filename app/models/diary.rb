d0fdacb67d9eb05765b3b506f7a7ea47f1ae5534
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2015-04-20 11:20:55 +0700

    put the title of diary into the url and replace space with dash

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,4 +2,13 @@ class Diary < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  #make dash urls
+  def slug
+    title.downcase.gsub(" ", "-")
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
 end
